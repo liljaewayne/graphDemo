5fd25cb876afb2892b1b727f0144060a6d194578
liujianwei <792068007@qq.com>
2022-07-18 15:15:49 +0800

    init

---
+++ src/test/java/com/ljw/graph/GraphDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljw.graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
